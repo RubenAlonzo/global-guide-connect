853915511c6a16354023c3e6fd1020d6a5e724b7
gabriel almonte <gabrielalmonte@hotmail.es>
2024-07-25 19:40:57 -0400

    AddPlaces

--- globe-guide-connect/src/App.js
+++ globe-guide-connect/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
+import FormPlaces from './pages/FormPlaces';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
@@ -8,6 +10,8 @@ const App = () => {
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="*" element={<Navigate to="/home" />} />
+        <Route path="/FromPlaces" element={<FormPlaces />} />
+        <Route path="*" element={<Navigate to="/FormPlaces" />} />
       </Routes>
     </Router>
   );
