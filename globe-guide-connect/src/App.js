16207d2f4af236be37c7da57d878336f5839b5c1
Diego-Oz <ortizdiego1507@gmail.com>
2024-07-24 23:55:05 -0400

    Navigation routes

--- globe-guide-connect/src/App.js
+++ globe-guide-connect/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
+import Guides from './pages/Guides'
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="*" element={<Navigate to="/home" />} />
+        <Route path="/guides" element={<Guides />} />
       </Routes>
     </Router>
   );
--- globe-guide-connect/src/components/Header.js
+++ globe-guide-connect/src/components/Header.js
@@ -8,8 +8,8 @@ const Header = () => {
           <h1 className="logo">GG Connect</h1>
           <nav>
             <ul className="nav">
-              <li className="nav-item"><a className="nav-link" href="#">Home</a></li>
-              <li className="nav-item"><a className="nav-link" href="#">Places</a></li>
+              <li className="nav-item"><a className="nav-link" href="/">Home</a></li>
+              <li className="nav-item"><a className="nav-link" href="/guides">Places</a></li>
               <li className="nav-item"><a className="nav-link" href="#">Guides</a></li>
               <li className="nav-item"><a className="nav-link" href="#">Sign In</a></li>
               <li className="nav-item"><a className="btn btn-success text-white" href="#">Sign Up</a></li>
